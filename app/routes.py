c7627dc09839ce036fb39e57d1258117cad3d8f1
Renan Tomasi <rnantg21@gmail.com>
2024-04-29 14:43:30 -0300

    add

--- app/routes.py
+++ app/routes.py
@@ -4,7 +4,7 @@ from flask import render_template
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index_1.html')
+    return render_template('index.html')
 
 @app.route('/sobre')
 def sobre():
